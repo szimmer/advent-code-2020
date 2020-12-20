9c2b0b7953bac40a26ded10a48df3f6d7c3a55fa
Stephanie <stephaniezimmer@gmail.com>
2020-12-20 07:57:42 -0500

    Day 15, P2

--- Scripts/Day15_Part2.R
+++ Scripts/Day15_Part2.R
@@ -1,29 +1,30 @@
+library(hash)
+
+
 input <- c(18,11,9,0,5,1)
 
-occs <- vector("list", length=length(input))
-names(occs) <- input
-for (i in 1:length(input)){
-  occs[[i]] <- i
-}
+occs <- hash(values=1:length(input), keys=input)
 
 prevval <- input[length(input)]
 
-for (i in as.integer((length(input)+1):30000000)){
-
+for (i in (length(input)+1):30000000){
+  if (i %% 10000==0) print(i)
   prevtimes <- occs[[as.character(prevval)]]
   
   if (length(prevtimes)>1){
     prevval <- as.integer(abs(diff(prevtimes)))
-    if (is.null(occs[[as.character(prevval)]])){
+    if (!has.key(as.character(prevval), occs)){
       occs[[as.character(prevval)]] <- i
     } else{
-      occs[[as.character(prevval)]] <- c(occs[[as.character(prevval)]][length(occs[[as.character(prevval)]])], i)  
+      occsi <- occs[[as.character(prevval)]]
+      occs[[as.character(prevval)]] <- c(occsi[length(occsi)], i)  
     }
     
     #next val
   } else{
     prevval <- 0
-    occs[["0"]] <- c(occs[["0"]][length(occs[["0"]])], i)
+    occs0 <- occs[["0"]]
+    occs[["0"]] <- c(occs0[length(occs0)], i)
   }
 }
 
